59041bbcb1d71e6ccbc9041035f6755fc40c1c69
Prudent Muthoni <muthonimprudent@gmail.com>
2026-06-18 16:52:46 +0300

    Styled the like button

--- app/components/CardGrid.js
+++ app/components/CardGrid.js
@@ -6,6 +6,7 @@ export default function CardGrid({ items }) {
   return (
     <div className={styles.grid}>
       {items.map((item) => (
+        <Link href={"/faves/" + item.id} key={item.id} style={{textDecoration: "none", color: "inherit"}}>
         <Card
           key={item.id}
           name={item.name}
@@ -13,10 +14,8 @@ export default function CardGrid({ items }) {
           rating={item.rating}
           image={item.image}
         />
+        </Link>
       ))}
     </div>
   );
-  <Link href={"/faves/" + item.id} key={item.id}>
-  <Card name={item.name} blurb={item.blurb} rating={item.rating} emoji={item.emoji} />
-</Link>
 }
--- app/components/LikeButton.js
+++ app/components/LikeButton.js
@@ -6,8 +6,8 @@ export default function LikeButton() {
   const [liked, setLiked] = useState(false);
 
   return (
-    <button onClick={() => setLiked(!liked)}>
-      {liked ? "❤️ Liked" : "🤍 Like"}
+    <button onClick={() => setLiked(!liked)} style={{width: "150px", height:"32px" , borderRadius:"10px",backgroundColor: "#8D021F", color:"White"}}>
+      {liked ? "❤️ Liked" : "🤍 Like"} 
     </button>
   );
 }
