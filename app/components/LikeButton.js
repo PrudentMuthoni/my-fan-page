3df31a4d84dab8908b0d60322650b823cd1b3a61
Prudent Muthoni <muthonimprudent@gmail.com>
2026-06-18 17:22:09 +0300

    Changed the color of the like button

--- app/components/LikeButton.js
+++ app/components/LikeButton.js
@@ -6,7 +6,7 @@ export default function LikeButton() {
   const [liked, setLiked] = useState(false);
 
   return (
-    <button onClick={() => setLiked(!liked)} style={{width: "150px", height:"32px" , borderRadius:"10px",backgroundColor: "#8D021F", color:"White"}}>
+    <button onClick={() => setLiked(!liked)} style={{width: "150px", height:"32px" , borderRadius:"10px",backgroundColor: " #420D09", color:"White", fontSize: "15px"}}>
       {liked ? "❤️ Liked" : "🤍 Like"} 
     </button>
   );
